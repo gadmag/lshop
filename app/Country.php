603ae49ae5afe2b383701324f9678f8add0390b7
gadmag <gadjim4@gmail.com>
2019-07-12 17:36:25 +0300

    Упорядочил список областей на странице заказа

--- app/Country.php
+++ app/Country.php
@@ -3,10 +3,21 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
+
 
 class Country extends Model
 {
     protected $table = 'country';
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::addGlobalScope('sort_name', function (Builder $builder) {
+            $builder->orderBy('name','asc');
+        });
+    }
 
     public function scopeActive($query)
     {
--- app/Region.php
+++ app/Region.php
@@ -3,8 +3,20 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
+
 
 class Region extends Model
 {
     protected $table = 'region';
+
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::addGlobalScope('sort_name', function (Builder $builder) {
+            $builder->orderBy('name','asc');
+        });
+    }
 }
