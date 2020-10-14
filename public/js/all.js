937439cc5868ff0c26bbbcad17116ac46969eb48
Gaji <gadjim4@gmail.com>
2020-10-14 04:57:05 +0300

    Style(upload) input box file component

--- public/js/all.js
+++ public/js/all.js
@@ -1,1 +1,93 @@
-$(document).ready(function(){$(".first-button").on("click",function(){$(".animated-icon").toggleClass("open")}),$("ul.navbar-nav li.dropdown").hover(function(){$(this).addClass("show"),$(this).find(".dropdown-menu").addClass("show")},function(){$(this).removeClass("show"),$(this).find(".dropdown-menu").removeClass("show")}),$("ul.navbar-nav li.dropdown").on("click",function(){var o=$(this);if(o.hasClass("show")){var n=o.children("a");n.length&&n.attr("href")&&(location.href=n.attr("href"))}}),$(".block-forms a.btn").click(function(o){o.preventDefault(),$(".block-forms a.active").removeClass("active"),$(this).addClass("active");var n=$(this).attr("href");$(".form-content").not(n).css({display:"none"}),$(n).fadeIn(400)});var o=function(o,n,t){var a=o.outerHeight(),i=$("header").outerHeight();n.scrollTop()>=i?(t.height(a),o.addClass("affix")):(o.removeClass("affix"),t.height("auto"))};$('[data-toggle="affix"]').each(function(){var n=$(this),t=$("<div></div>");n.before(t),$(window).on("scroll resize",function(){o(n,$(this),t)}),o(n,$(window),t)}),$(".cart-item-detail .dropdown-menu.cart-block").on("click",function(o){o.stopPropagation()}),$("#engravingDetail.dropdown").hover(function(){$(this).addClass("show"),$(this).find(".dropdown-menu").addClass("show")},function(){$(this).removeClass("show"),$(this).find(".dropdown-menu").removeClass("show")}),$('[data-toggle="tooltip"]').tooltip()});
+
+$(document).ready(function () {
+    $('.first-button').on('click', function () {
+        $('.animated-icon').toggleClass('open');
+    });
+    $('ul.navbar-nav li.dropdown').hover(function () {
+        $(this).addClass('show');
+        $(this).find('.dropdown-menu').addClass('show');
+
+    }, function () {
+        $(this).removeClass('show');
+        $(this).find('.dropdown-menu').removeClass('show');
+    });
+
+    $('ul.navbar-nav li.dropdown').on('click', function() {
+        var $el = $(this);
+        if ($el.hasClass('show')) {
+            var $a = $el.children('a');
+            if ($a.length && $a.attr('href')) {
+                  location.href = $a.attr('href');
+            }
+        }
+    });
+
+    $('.block-forms a.btn').click(function (e) {
+        e.preventDefault();
+        $('.block-forms a.active').removeClass('active');
+        $(this).addClass('active');
+        var tab = $(this).attr('href');
+        $('.form-content').not(tab).css({'display': 'none'});
+        $(tab).fadeIn(400);
+    });
+
+    var toggleAffix = function(affixElement, scrollElement, wrapper) {
+        var height = affixElement.outerHeight(),
+            top = $('header').outerHeight();
+        // console.log(top);
+            // top = wrapper.offset().top;
+
+        if (scrollElement.scrollTop() >= top){
+              wrapper.height(height);
+            affixElement.addClass("affix");
+        }
+        else {
+            affixElement.removeClass("affix");
+            wrapper.height('auto');
+        }
+
+    };
+
+
+    $('[data-toggle="affix"]').each(function() {
+        var ele = $(this),
+            wrapper = $('<div></div>');
+
+        ele.before(wrapper);
+        $(window).on('scroll resize', function() {
+            toggleAffix(ele, $(this), wrapper);
+        });
+
+        // init
+        toggleAffix(ele, $(window), wrapper);
+    });
+
+
+
+    $('.cart-item-detail .dropdown-menu.cart-block').on('click', function(event) {
+        event.stopPropagation();
+    });
+
+    $('#engravingDetail.dropdown').hover(function () {
+        $(this).addClass('show');
+        $(this).find('.dropdown-menu').addClass('show');
+
+    }, function () {
+        $(this).removeClass('show');
+        $(this).find('.dropdown-menu').removeClass('show');
+    });
+
+    //tooltip
+    $('[data-toggle="tooltip"]').tooltip()
+});
+
+
+
+
+
+// $(window).on('load', function () {
+//     var $preloader = $('.holder'),
+//         $spinner   = $preloader.find('.preloader');
+//     $spinner.fadeOut();
+//     $preloader.delay(350).fadeOut('slow');
+// });
