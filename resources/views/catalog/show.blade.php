03ca41b71d5dfaaf8f786755fe8d9b693b7d5f76
Gaji <gadjim4@gmail.com>
2021-03-08 18:28:47 +0300

    Fix(products, catalog) breadcrumb add catalog link

--- resources/views/catalog/show.blade.php
+++ resources/views/catalog/show.blade.php
@@ -12,16 +12,18 @@
     <meta property="og:image" content=""/>
 @endsection
 @section('content')
-<div class="container">
+    <div class="container">
 
-    <nav aria-label="breadcrumb" role="navigation">
-        <br>
-        <ol class="breadcrumb">
-            <li class="breadcrumb-item"><a href="/">Главная</a></li>
-            <li class="breadcrumb-item"><a href="{{route('product.index')}}">Каталог товаров</a></li>
-            <li class="breadcrumb-item active" aria-current="page">{{$category->name}}</li>
-        </ol>
-    </nav>
-    <catalog-index :category="{{$category}}" :filters="{{$filters}}"></catalog-index>
-</div>
+        <nav aria-label="breadcrumb" role="navigation">
+            <br>
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="/">Главная</a></li>
+                @if($category->parent()->exists())
+                    <li class="breadcrumb-item"><a href="{{$category->parent()->first()->path}}">{{$category->parent()->first()->name}}</a></li>
+                @endif
+                <li class="breadcrumb-item active" aria-current="page">{{$category->name}}</li>
+            </ol>
+        </nav>
+        <catalog-index :category="{{$category}}" :filters="{{$filters}}"></catalog-index>
+    </div>
 @endsection
--- resources/views/menu/menu_item.blade.php
+++ resources/views/menu/menu_item.blade.php
@@ -1,24 +1,20 @@
-<li class="nav-item {{(count($menu->children) > 0)? 'dropdown': ''}} {{$loop->first?'first':''}} {{$loop->last?'last':''}}"
+<li class="nav-item {{(count($menu->children) > 0)? 'dropdown': ''}} {{$loop->first?'first':''}} {{$loop->last?'last':''}} {{$menu->class?:''}}"
     data-depth="{{$menu['depth']}}">
 
     @if(count($menu->children) > 0)
-        <div class="menu-element {{$menu->class?:''}}">
-            @if($menu->path == '<nolink>')
-                <span class="nolink">{{$menu->title}}</span>
-            @else
-                <a class="nav-link" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"
-                   href="{{$menu->link_path}}">{{$menu->title}}</a>
-            @endif
-        </div>
+        @if($menu->path == '<nolink>')
+            <span class="nolink">{{$menu->title}}</span>
+        @else
+            <a class="nav-link" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"
+               href="{{$menu->link_path}}">{{$menu->title}}</a>
+        @endif
     @else
-        <div class="menu-element {{$menu->class?:''}}">
-            @if($menu->path == '<nolink>')
-                <span class="nolink">{{$menu->title}}</span>
-            @else
-                <a class="nav-link @if($menu->parent_id > 0){{'dropdown-item'}}@endif"
-                   href="{{$menu->link_path}}">{{$menu->title}}</a>
-            @endif
-        </div>
+        @if($menu->path == '<nolink>')
+            <span class="nolink">{{$menu->title}}</span>
+        @else
+            <a class="nav-link @if($menu->parent_id > 0){{'dropdown-item'}}@endif"
+               href="{{$menu->link_path}}">{{$menu->title}}</a>
+        @endif
     @endif
     @if(count($menu->children) > 0)
         <ul class="dropdown-menu">
